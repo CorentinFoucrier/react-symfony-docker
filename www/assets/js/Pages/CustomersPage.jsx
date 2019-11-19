5fd87fabec949817ceec26a1b90647729a0a9cfc
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-11-19 11:05:55 +0100

    Appel API

--- www/assets/js/Pages/CustomersPage.jsx
+++ www/assets/js/Pages/CustomersPage.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import Axios from 'axios';
+import {useState,useEffect} from 'react';
 
 const CustomersPage = (props) => {
+    const [Customer, setCustomer] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:8282/api/customers')
+        .then(response => response.data['hydra:member'])
+        .then(data => setCustomer(data))
+        .catch(error => console.log(error.response));
+    }, []);
+
     return (
         <>
             <h1>Liste des clients</h1>
-            <table className="table table-striped">
+            <table className="table table-striped table-bordered">
                 <thead className="thead-dark">
                     <tr>
                         <th>ID</th>
@@ -13,19 +24,23 @@ const CustomersPage = (props) => {
                         <th>Société</th>
                         <th className="text-center">Facture</th>
                         <th className="text-center">Montant total facturé</th>
-                        <th></th>
+                        <th>Suppr.</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <td>1</td>
-                    <td>
-                        <a href="1">Le bob</a>
-                    </td>
-                    <td>lebob@react.com</td>
-                    <td>LeBob Incorporation</td>
-                    <td className="text-center"><span className="badge badge-primary">4</span></td>
-                    <td className="text-center">2400,00€</td>
-                    <td><button className="btn btn-sm btn-danger">Supprimer</button></td>
+                    {Customer.map (customer =>
+                    <tr key={customer.id}>
+                        <td>{customer.id}</td>
+                        <td>
+                            <a href="1">{customer.firstName} {customer.lastName}</a>
+                        </td>
+                        <td>{customer.email}</td>
+                        <td>{customer.company}</td>
+                        <td className="text-center"><span className="badge badge-primary">{customer.invoices.length}</span></td>
+                        <td className="text-center">{customer.totalAmount.toLocaleString()}</td>
+                        <td><button className="btn btn-sm btn-danger"><i className="far fa-trash-alt"></i></button></td>
+                    </tr>
+                    )}
                 </tbody>
             </table>
         </>
